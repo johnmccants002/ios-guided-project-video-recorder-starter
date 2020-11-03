89126362f460858178147b6b9667d994d7a52128
johnmccants002 <johnmccants002@gmail.com>
2020-11-02 19:30:52 -0800

    Added file output. Worked on recording button tapped. Conformed to delegate.

--- VideoRecorder/VideoRecorder/CameraViewController.swift
+++ VideoRecorder/VideoRecorder/CameraViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 import AVFoundation
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
     
     lazy private var captureSession = AVCaptureSession()
+    lazy private var fileOutput = AVCaptureMovieFileOutput()
 
     @IBOutlet var recordButton: UIButton!
     @IBOutlet var cameraView: CameraPreviewView!
@@ -24,6 +25,18 @@ class CameraViewController: UIViewController {
 		// Resize camera preview to fill the entire screen
 		cameraView.videoPlayerLayer.videoGravity = .resizeAspectFill
 	}
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        captureSession.startRunning()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        captureSession.stopRunning()
+    }
     
     private func setUpCamera() {
         let camera = bestCamera
@@ -38,7 +51,15 @@ class CameraViewController: UIViewController {
             preconditionFailure("Unable to add camera input for capture session")
         }
         
+        guard captureSession.canAddOutput(fileOutput) else {
+            preconditionFailure("Can't add fileOutput. Can't handle this type of output \(fileOutput)")
+        }
+        captureSession.addOutput(fileOutput)
         captureSession.addInput(cameraInput)
+        
+        if captureSession.canSetSessionPreset(.hd1920x1080) {
+            captureSession.sessionPreset = .hd1920x1080
+        }
         
         captureSession.commitConfiguration()
     }
@@ -55,8 +76,12 @@ class CameraViewController: UIViewController {
     }
 
 
-    @IBAction func recordButtonPressed(_ sender: Any) {
-
+    @IBAction func toggleRecording(_ sender: Any) {
+        if fileOutput.isRecording {
+            fileOutput.stopRecording()
+        } else {
+            fileOutput.startRecording(to: newRecordingURL(), recordingDelegate: self)
+        }
 	}
 	
 	/// Creates a new file URL in the documents directory
